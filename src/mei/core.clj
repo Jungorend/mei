562a55630439ecaecffca88bf62df4716a0f1ef0
Jungy <jungorend@gmail.com>
2016-11-04 22:44:59 -0400

    Added basic packet types

--- src/mei/core.clj
+++ src/mei/core.clj
@@ -1,4 +1,4 @@
-(ns scratchpad
+(ns mei.core
   (:require [clojure.string :as str]))
 
 (defn third [a]
@@ -23,6 +23,10 @@
                 (cond hex? data
                       :else (format (str "%" length "x") data))) values)) #" " "0"))
 
+(defn text-to-hex
+  "Creates the hex equivalent of a text string"
+  [text]
+  (apply convert-to-hex (map vector (map int text) (repeat 2))))
 ;;----
 
 (def link-types
@@ -32,6 +36,24 @@
    :ipv4 228
    :ipv6 229
    })
+
+(def ether-types
+  {:ipv4 "0800"
+   :arp  "0806"
+   :ipv6 "86DD"})
+
+(def ip-protocols
+  {:icmp "01"
+   :udp "11"
+   :tcp "06"})
+
+(def timestamp (atom (rand-int 15129)))
+(def source-port (atom (+ 1024 (rand-int 1000))))
+(def mac-dst "aaaaaaaaaaaa")
+(def mac-src "bbbbbbbbbbbb")
+(def src-ip "0aaaaaaa")
+(def dst-ip "0aaaaaab")
+(def filename "test.pcap")
 
 (defn global-header [link]
   "Takes a link type and produces the global header for a pcap as a string"
@@ -44,7 +66,58 @@
     [(get link-types link) 8]))
 
 (defn record-header [ts_sec ts_usec incl_len orig_len]
-  (convert-to-hex [ts_sec 8]
-                   [ts_usec 8]
-                   [incl_len 8]
-                   [orig_len 8]))
+  (convert-to-hex [ts_sec 8]     ; timestamp seconds
+                  [ts_usec 8]    ; timestamp microseconds
+                  [incl_len 8]   ; number of octets of packet saved in file
+                  [orig_len 8])) ; actual length of packet
+
+(defn create-record-header [data]
+  (swap! timestamp #(+ % 1))
+  (let [len (/ (count data) 2)]
+    (record-header @timestamp 0 len len)))
+
+(defn create-packet [data]
+  (print-hexstring filename (str (create-record-header data) data)))
+
+;;---------
+
+(defn ethernet-packet [type data]
+  (convert-to-hex [mac-dst 12 true]
+                  [mac-src 12 true]
+                  [(get ether-types type) 4 true]
+                  [data 0 true]
+                  ["c704dd7b" 8 true]    ;; TODO: Actual FCS checki
+                  ))
+
+(defn ipv4-packet [protocol src-ip dst-ip data]
+  (convert-to-hex ["4" 1 true]
+                  ["5" 1 true] ;; TODO: Maybe add IP options?
+                  ["00" 2 true] ;; TODO: VoIP?
+                  [(+ 20 (/ (count data) 2)) 4] ;;size of the entire packet
+                  [0 4]         ;; TODO: Basically everything here
+                  [0 4]
+                  [15 2]
+                  [(get ip-protocols protocol) 2 true]
+                  [0 4] ;; TODO: More Checksums....
+                  [src-ip 8 true]
+                  [dst-ip 8 true]
+                  [data 0 true]))
+
+
+
+(defn udp-packet [dport data]
+  (convert-to-hex [@source-port 4]
+                  [dport 4]
+                  [(/ (count data) 2) 4]
+                  [0 4]
+                  [data 0 true]))                    ;; TODO: No checksum validation yet. Will add later
+
+
+
+
+
+
+
+
+
+(print-hexstring filename (global-header :ethernet))
